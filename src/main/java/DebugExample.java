af67a0497b3c05f1a2bb8de99d748e26299d93ce
Liytio <rprasytio@gmail.com>
2024-10-23 08:23:50 +0700

    commit1

---
+++ src/main/java/DebugExample.java
@@ -0,0 +1,8 @@
+public class DebugExample {
+    public static void main(String[] args){
+        int x = x;
+        int y = y;
+        int sum = x+y;
+        System.out.println("Sum : " + sum);
+    }
+}
